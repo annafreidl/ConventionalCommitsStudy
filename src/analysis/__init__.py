0961ef1ae2fe82fd91bba59d77cff02bac3f8dab
annafreidl <annafreidl@yahoo.de>
2024-09-18 14:29:52 +0200

    chore: Remove unnecessary file
    Deleted custom_types_analysis.py from the project as it is no longer needed. It is now included in the JSON of each project.

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -1,12 +1,9 @@
 # source/analysis/__init__.py
 from .conventional_commits import is_conventional_commit, custom_types, is_conventional_custom, get_commit_type
-from .custom_types_analysis import save_custom_types, CUSTOM_TYPES_FILE_PATH
 
 __all__ = [
     'is_conventional_commit',
     'custom_types',
-    'save_custom_types',
-    'CUSTOM_TYPES_FILE_PATH',
     'is_conventional_custom',
     'get_commit_type'
 ]
